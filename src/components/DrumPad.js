ccece3354410dfea61e1c15066b45fc8e9e8f267
pjmantoss <joelptoss@gmail.com>
2020-04-03 16:17:52 +0100

    update DrumPad

---
+++ src/components/DrumPad.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+const activeStyle = {
+    backgroundColor: "orange",
+    boxShadow: "0 3px orange",
+    height: 77,
+    marginTop: 13
+}
+
+const inactiveStyle = {}
+
+export class DrumPad extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default DrumPad
