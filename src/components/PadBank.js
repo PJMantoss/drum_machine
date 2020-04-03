10869b3a6aa6e6a813c800eca19dc9697fd74657
pjmantoss <joelptoss@gmail.com>
2020-04-03 17:01:30 +0100

    update PadBank

---
+++ src/components/PadBank.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PadBank extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default PadBank
