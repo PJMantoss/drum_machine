1002b2f2f1b17f60711207eca0455d56106f12c5
pjmantoss <joelptoss@gmail.com>
2020-04-03 17:33:28 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import bankTwo from './components/bankTwo';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      power: true,
+      display: String.fromCharCode(160),
+      currentPadBank: bankOne,
+      currentPadBankId: "Heater Kit",
+      sliderVal: 0.3
+    }
+  }
+  powerControl = () => {
+    this.setState({
+      power: !this.state.power,
+      display: String.fromCharCode(160)
+    })
+  }
+
+  selectBank = () => {
+    if(this.state.power){
+      this.state.currentPadBankId === "Heater Kit" ? 
+      this.setState({
+        currentPadBank: bankTwo,
+        display: "Smooth Piano Kit",
+        currentPadBankId: "Smooth Piano Kit"
+      }) : this.setState({
+        currentPadBank: bankOne,
+        display: "Heater Kit",
+        currentPadBankId: "Heater Kit"
+      })
+    }
+  }
+
+  displayClipName = () => {}
+
+  adjustVolume = () => {}
+
+  clearDisplay = () => {}
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
