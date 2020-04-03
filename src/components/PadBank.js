e3ae0e7a79ed7b551656ad7a1a684803d767d544
pjmantoss <joelptoss@gmail.com>
2020-04-03 17:08:33 +0100

    update PadBank

--- src/components/PadBank.js
+++ src/components/PadBank.js
@@ -1,7 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import DrumPad from "./components/DrumBeat"
 
 export class PadBank extends Component {
+    constructor(props){
+        super(props);
+    }
+
     render() {
+        let padBank;
+        this.props.power ? 
+        padBank = this.props.currentPadBank.map((drumObj, i, padBankArr) => {
+            return (
+                <DrumPad 
+                    clipId={} 
+                    clip={} 
+                    keyTrigger={} 
+                    keyCode={} 
+                    updateDisplay={} 
+                    power={} 
+                />
+            )
+        });
+
         return (
             <div>
                 
